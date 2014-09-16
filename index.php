ef4bd8424c3259f23e908adade605efb4ed2821f
rohit <rohitsinghal.rs@gmail.com>
2014-09-16 10:46:01 +0530

    0.1v

---
+++ index.php
@@ -0,0 +1,26 @@
+<h1> iOS Font Installer v0.1</h1>
+<form action='' method='POST' enctype='multipart/form-data'>
+<input type='file' name='userFile'> 
+<input type='submit' name='upload_btn' value='upload'>
+</form>
+
+<?php
+if(isset($_POST['upload_btn']))  {
+//$target_Path = $target_Path.basename( $_FILES['userFile']['name'] );
+ $info = pathinfo($_FILES['userFile']['name']);
+ //$ext = $info['extension']; // get the extension of the file
+ //var_dump($info);
+ $newname = $info['basename'];
+ $target = 'ttfs/'.$newname;
+move_uploaded_file( $_FILES['userFile']['tmp_name'], $target );
+}
+?>
+<hr>
+<h3>Uploaded Fonts</h3>
+<?php
+// var_dump(glob('ttfs/*'));
+foreach(glob('ttfs'.'/*.ttf') as $file) {
+    echo '<a href="install.php?font='.$file."\">$file</a>";
+}
+
+?>
---
+++ install.php
@@ -0,0 +1,47 @@
+<?php
+
+$file = $_GET['font'];
+$fontname = str_replace('.ttf', '', $file);
+$fontname = str_replace('ttfs/', '', $fontname);
+$fontname = str_replace(' ', '-', $fontname);
+$data = file_get_contents($file);
+$data = base64_encode($data);
+header('Content-disposition: attachment; filename="'.$fontname.'.mobileconfig"');
+header('Content-type: "text/xml"; charset="utf8"');
+echo "<?xml version='1.0' encoding='UTF-8'?>
+<!DOCTYPE plist PUBLIC '-//Apple//DTD PLIST 1.0//EN' 'http://www.apple.com/DTDs/PropertyList-1.0.dtd'>
+<plist version='1.0'>
+<dict>
+<key>PayloadContent</key>
+<array>
+<dict>
+	<key>Font</key>
+	<data>
+$data
+	</data>
+	<key>PayloadIdentifier</key>
+	<string>com.tb3.fonts.$fontname</string>
+	<key>PayloadType</key>
+	<string>com.apple.font</string>
+	<key>PayloadUUID</key>
+	<string>59145A6E-63C4-4B68-B0B3-1E4568CE121B_$fontname</string>
+	<key>PayloadVersion</key>
+	<integer>1</integer>
+</dict>
+</array>
+<key>PayloadDisplayName</key>
+<string>$fontname</string>
+<key>PayloadIdentifier</key>
+<string>com.tb3.fonts.$fontname</string>
+<key>PayloadOrganization</key>
+<string>tb3.co.in</string>
+<key>PayloadType</key>
+<string>Configuration</string>
+<key>PayloadUUID</key>
+<string>59145A6E-63C4-4B68-B0B3-1E4568CE121B_$fontname</string>
+<key>PayloadVersion</key>
+<integer>1</integer>
+<key>DurationUntilRemoval</key>
+<integer>2</integer>
+</dict>
+</plist>";
